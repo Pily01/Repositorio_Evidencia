11e8ca332543c1e4f07b78bc264a8ba7af5a399b
NaomiToledo <80797990+NaomiToledo@users.noreply.github.com>
2021-03-18 12:16:17 -0600

    Update JuegoPintado.py

--- JuegoPintado.py
+++ JuegoPintado.py
@@ -32,16 +32,10 @@ def square(start, end):
         left(90)
 
     end_fill()
-
+    
 def circle(start, end):
-    Draw a circle from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-for count in range(4):
-        forward(end.x - start.x)
-        left(90)
+    "Draw circle from start to end."
+    pass  # TODO
         
 def rectangle(start, end):
     "Draw rectangle from start to end."
