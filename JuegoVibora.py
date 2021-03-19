e2f273643a8acb642ed4cd4899e81e8a38d6c062
Pily Domo <40185631+Pily01@users.noreply.github.com>
2021-03-19 08:28:45 -0600

    Update JuegoVibora.py
    Add names of authors

--- JuegoVibora.py
+++ JuegoVibora.py
@@ -2,10 +2,10 @@
 
 Exercises
 
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to arrow keys.
+Autor@s:
+María del Pilar Domínguez Molina
+Naomi Alejandra Toledo
+Ameyalli Sánchez Mendoz
 
 """
 
