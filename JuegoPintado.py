4474b0e1d09111af2098177f4c3340619357a0ed
Pily Domo <40185631+Pily01@users.noreply.github.com>
2021-03-19 08:28:05 -0600

    Update JuegoPintado.py
    Add names of authors

--- JuegoPintado.py
+++ JuegoPintado.py
@@ -1,14 +1,10 @@
 
 """Paint, for drawing shapes.
 
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter. 
-
+Autor@s:
+María del Pilar Domínguez Molina
+Naomi Alejandra Toledo
+Ameyalli Sánchez Mendoza
 """
 
 from turtle import *
